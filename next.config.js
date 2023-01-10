8a342eb625faad81982d6b353ccb89e8199079c8
stfluckly29 <stefanklein1124@gmail.com>
2023-01-10 22:27:42 +0900

    fix: next.config.js

--- next.config.js
+++ next.config.js
@@ -1,29 +1,15 @@
 /**
  * @type {import('next').NextConfig}
  */
-const isProd = process.env.NODE_ENV === 'production'
-
 module.exports = {
   swcMinify: false,
-  assetPrefix: isProd ? '/stfluckly29.github.io/' : '',
   experimental: {
     scrollRestoration: true,
     nextScriptWorkers: true,
   },
   images: {
-    unoptimized: true,
+    loader: 'akamai',
+    path: '',
   },
-
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      });
-    }
-
-    return config;
-  },
+  assetPrefix: './',
 };
